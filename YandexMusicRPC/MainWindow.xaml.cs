61746f747f22ba867363afafa4e8a21d1a08e483
Yanva <54475167+yanvaa@users.noreply.github.com>
2022-12-30 21:28:55 +0400

    Add files via upload

---
+++ YandexMusicRPC/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using DiscordRPC;
+using System;
+using System.Windows;
+using System.Windows.Threading;
+using System.Diagnostics;
+
+
+namespace YandexMusicRPC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DiscordRpcClient client = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            client = new DiscordRpcClient("1011365853645787227");
+            client.Initialize();
+            var timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000);
+            timer.Start();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Change();
+        }
+        private async void Change()
+        {
+            try
+            {
+                MusicData data = await new MusicData().GetData();
+                OutputText.Text = data.Title + " " + data.Artist;
+                OutputImage.Source = data.Thumbnail;
+                client.SetPresence(new RichPresence()
+                {
+                    Details = data.Title,
+                    State = data.Artist,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "image_large"
+                    },
+                    Buttons = new Button[]
+                    {
+                    new Button() { Label = "Послушать", Url = data.url }
+                    }
+                });
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void StopRPC_Click(object sender, RoutedEventArgs e)
+        {
+            client.Deinitialize();
+            client.Invoke();
+            StopRPC.IsEnabled = false;
+            StartRPC.IsEnabled = true;
+        }
+
+        private void StartRPC_Click(object sender, RoutedEventArgs e)
+        {
+            client = null;
+            client = new DiscordRpcClient("1011365853645787227");
+            client.Initialize();
+            client.Invoke();
+            StopRPC.IsEnabled = true;
+            StartRPC.IsEnabled = false;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Visibility = Visibility.Hidden;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            string link = e.Uri.AbsoluteUri;
+            Process.Start(new ProcessStartInfo("cmd", $"/c start " + link));
+            e.Handled = true;
+        }
+    }
+}
---
+++ YandexMusicRPC/MusicData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Windows.Media.Control;
+using Windows.Storage.Streams;
+using YandexMusicApi.Client;
+
+namespace YandexMusicRPC
+{
+    class MusicData
+    {
+        public string Title;
+        public string Artist;
+        public BitmapImage Thumbnail;
+        public string url;
+        private GlobalSystemMediaTransportControlsSessionMediaProperties mediaProperties;
+
+        public MusicData() { }
+        public MusicData(string title, string artist, BitmapImage image, string url_) { Title = title; Artist = artist; Thumbnail = image; url = url_; }
+
+        public async Task<MusicData> GetData()
+        {
+            var client = new YandexMusicClient();
+            var gsmtcsm = await GetSystemMediaTransportControlsSessionManager();
+            var mediaProperties = await GetMediaProperties(gsmtcsm.GetCurrentSession());
+            Title = mediaProperties.Title;
+            Artist = mediaProperties.Artist;
+            var thumbnail = await mediaProperties.Thumbnail.OpenReadAsync();
+            BitmapImage thumb;
+            using (DataReader dataReader = new DataReader(thumbnail))
+            {
+                var bytes = new byte[thumbnail.Size];
+                await dataReader.LoadAsync((uint)thumbnail.Size);
+                dataReader.ReadBytes(bytes);
+                var image = new BitmapImage();
+                using (var ms = new System.IO.MemoryStream(bytes))
+                {
+                    image.BeginInit();
+                    image.CacheOption = BitmapCacheOption.OnLoad; // here
+                    image.StreamSource = ms;
+                    image.EndInit();
+                }
+                thumb = image;
+            }
+            Thumbnail = thumb;
+            var track = await client.Tracks.SearchAsync(Title + " " + Artist);
+            if (track != null)
+            {
+                var result = track.Results.ElementAt(0);
+                return new MusicData(Title, Artist, Thumbnail, $"https://music.yandex.ru/track/{result.RealId}");
+            }
+
+            return new MusicData(Title, Artist, Thumbnail, $"https://github.com/yanvaa/YandexMusicRPC");
+        }
+
+        private static async Task<GlobalSystemMediaTransportControlsSessionManager> GetSystemMediaTransportControlsSessionManager() =>
+        await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+
+        private static async Task<GlobalSystemMediaTransportControlsSessionMediaProperties> GetMediaProperties(GlobalSystemMediaTransportControlsSession session) =>
+            await session.TryGetMediaPropertiesAsync();
+    }
+}
